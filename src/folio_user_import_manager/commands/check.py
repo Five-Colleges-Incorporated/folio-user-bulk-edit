a0de798443c45dbeadf6855507a3946cab610e1e
Katherine Bargar <kbargar@fivecolleges.edu>
2025-03-25 16:37:15 +0000

    Refactor check method to prepare for data checks

--- src/folio_user_import_manager/commands/check.py
+++ src/folio_user_import_manager/commands/check.py
@@ -5,6 +5,8 @@ import json
 import socket
 from dataclasses import dataclass
 
+import pandera.polars as pla
+
 
 @dataclass
 class CheckOptions:
@@ -28,39 +30,57 @@ class CheckResults:
     """The error (if there is one) connecting to FOLIO during the check."""
     folio_error: str | None = None
 
+    @property
+    def data_ok(self) -> bool:
+        """Is the data valid?"""
+        return self.data_errors is None
+
+    """The errors (if there are any) with the data."""
+    data_errors: pla.errors.SchemaErrors | None = None
+
 
 def run(options: CheckOptions) -> CheckResults:
-    """Checks for connectivity."""
-    folio = http.client.HTTPSConnection(options.folio_url)
-    try:
-        folio.request(
-            "POST",
-            "/authn/login-with-expiry",
-            json.dumps(
+    """Checks for connectivity and data validity."""
+    folio_error = None
+    while True:
+        folio = http.client.HTTPSConnection(options.folio_url)
+        try:
+            folio.request(
+                "POST",
+                "/authn/login-with-expiry",
+                json.dumps(
+                    {
+                        "username": options.folio_username,
+                        "password": options.folio_password,
+                    },
+                ),
                 {
-                    "username": options.folio_username,
-                    "password": options.folio_password,
+                    "x-okapi-tenant": options.folio_tenant,
+                    "content-type": "application/json",
                 },
-            ),
-            {
-                "x-okapi-tenant": options.folio_tenant,
-                "content-type": "application/json",
-            },
-        )
-    except socket.gaierror:
-        return CheckResults("Invalid FOLIO Url")
+            )
+        except socket.gaierror:
+            folio_error = "Invalid FOLIO Url"
+            break
 
-    res = folio.getresponse()
-    if res.status == 201 and "folioAccessToken" in res.getheader("set-cookie", ""):
-        return CheckResults()
+        res = folio.getresponse()
+        if res.status == 201 and "folioAccessToken" in res.getheader("set-cookie", ""):
+            break
 
-    if res.status == 405:
-        return CheckResults("Invalid FOLIO Services Url")
+        if res.status == 405:
+            folio_error = "Invalid FOLIO Services Url"
+            break
 
-    body = res.read().decode()
-    try:
-        reason = json.loads(body)["errors"][0]["code"]
-    except (ValueError, KeyError):
-        reason = body
+        body = res.read().decode()
+        try:
+            folio_error = json.loads(body)["errors"][0]["code"]
+        except (ValueError, KeyError):
+            folio_error = body
 
-    return CheckResults(reason)
+        break
+
+    data_errors = None
+    while True:
+        break
+
+    return CheckResults(folio_error, data_errors)
