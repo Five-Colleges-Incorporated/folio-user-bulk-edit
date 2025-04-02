afeb411d63217899f9d1f07ad484c8fd5fdc3fc2
Katherine Bargar <kbargar@fivecolleges.edu>
2025-04-02 16:54:24 +0000

    Check environment variable overrides

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,32 +1,71 @@
 import shlex
 import typing
+from contextlib import contextmanager
 from dataclasses import dataclass
+from pathlib import Path
 from unittest import mock
 
 from pytest_cases import parametrize_with_cases
+
+from folio_user_import_manager.commands.check import CheckOptions
 
 
 @dataclass
 class CliArgCase:
     args: str
+    envs: dict[str, str]
     _getpass: str
     expected_result: int
-    expected_calls: int
+    expected_options: CheckOptions
 
-    def patch_getpass(self) -> typing.Any:
-        return mock.patch(
-            "getpass.getpass",
-            return_value=self._getpass,
-        )
+    @contextmanager
+    def setup(self) -> typing.Any:
+        with (
+            mock.patch(
+                "getpass.getpass",
+                return_value=self._getpass,
+            ),
+            mock.patch.dict("os.environ", self.envs, clear=True),
+        ):
+            yield
 
 
 class CliArgCases:
-    def case_ok(self) -> CliArgCase:
+    def case_args_ok(self) -> CliArgCase:
         return CliArgCase(
             "check -e http://folio.org -t tenant -u user -p ./",
+            {},
             "pass",
             0,
-            1,
+            CheckOptions("folio.org", "tenant", "user", "pass", Path("./")),
+        )
+
+    def case_env_ok(self) -> CliArgCase:
+        return CliArgCase(
+            "check ./",
+            {
+                "FUIMAN__FOLIO__ENDPOINT": "http://folio.org",
+                "FUIMAN__FOLIO__TENANT": "tenant",
+                "FUIMAN__FOLIO__USERNAME": "user",
+                "FUIMAN__FOLIO__PASSWORD": "pass",
+            },
+            "",
+            0,
+            CheckOptions("folio.org", "tenant", "user", "pass", Path("./")),
+        )
+
+    def case_env_override(self) -> CliArgCase:
+        return CliArgCase(
+            "check -u another_user ./",
+            {
+                "FUIMAN__FOLIO__ENDPOINT": "http://folio.org",
+                "FUIMAN__FOLIO__TENANT": "tenant",
+                "FUIMAN__FOLIO__USERNAME": "user",
+                "FUIMAN__FOLIO__PASSWORD": "pass",
+            },
+            "",
+            0,
+            CheckOptions("folio.org", "tenant", "another_user", "pass", Path("./")),
         )
 
 
@@ -38,8 +77,8 @@ def test_cli_args(
 ) -> None:
     import folio_user_import_manager.cli as uut
 
-    with tc.patch_getpass():
+    with tc.setup():
         res = uut.main(shlex.split(tc.args))
 
     assert res == tc.expected_result
-    assert check_run_mock.call_count == tc.expected_calls
+    check_run_mock.assert_called_with(tc.expected_options)
