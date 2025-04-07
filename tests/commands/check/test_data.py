6b263db7da7a0ce0346bceb09a6920190c0a9a79
Katherine Bargar <kbargar@fivecolleges.edu>
2025-04-07 16:06:47 +0000

    Refactor data test cases to be dynamic

--- tests/commands/check/test_data.py
+++ tests/commands/check/test_data.py
@@ -1,52 +1,35 @@
-import pytest
+from pathlib import Path
+
+from pytest_cases import parametrize, parametrize_with_cases
+
+_samples = list((Path() / "tests" / "commands" / "check" / "samples").glob("*.csv"))
 
 
-def test_check_data_ok(request: pytest.FixtureRequest) -> None:
+class DataErrorCases:
+    @parametrize(csv=[s for s in _samples if "ok" in str(s)])
+    def case_ok(self, csv: Path) -> tuple[Path, bool, bool]:
+        return (csv, True, True)
+
+    @parametrize(csv=[s for s in _samples if "read" in str(s)])
+    def case_bad_read(self, csv: Path) -> tuple[Path, bool, bool]:
+        return (csv, True, False)
+
+    @parametrize(csv=[s for s in _samples if "schema" in str(s)])
+    def case_bad_schema(self, csv: Path) -> tuple[Path, bool, bool]:
+        return (csv, False, True)
+
+
+@parametrize_with_cases("path,read_ok,schema_ok", DataErrorCases)
+def test_check_data(path: Path, read_ok: bool, schema_ok: bool) -> None:  # noqa: FBT001
     import folio_user_import_manager.commands.check as uut
 
     res = uut.run(
-        uut.CheckOptions(
-            "",
-            "",
-            "",
-            "",
-            data_location=request.path.parent / "samples" / "ok.csv",
-        ),
+        uut.CheckOptions("", "", "", "", path),
     )
 
-    assert res.read_ok, str(res.read_errors["data"]) if res.read_errors else None
-    assert res.schema_ok, str(res.schema_errors["data"]) if res.schema_errors else None
-
-
-def test_check_schema_not_ok(request: pytest.FixtureRequest) -> None:
-    import folio_user_import_manager.commands.check as uut
-
-    res = uut.run(
-        uut.CheckOptions(
-            "",
-            "",
-            "",
-            "",
-            data_location=request.path.parent / "samples" / "schema_not_ok.csv",
-        ),
+    assert res.read_ok == read_ok, (
+        str(res.read_errors["data"]) if res.read_errors else None
     )
-
-    assert res.read_ok, str(res.read_errors["data"]) if res.read_errors else None
-    assert not res.schema_ok
-
-
-def test_check_read_not_ok(request: pytest.FixtureRequest) -> None:
-    import folio_user_import_manager.commands.check as uut
-
-    res = uut.run(
-        uut.CheckOptions(
-            "",
-            "",
-            "",
-            "",
-            data_location=request.path.parent / "samples" / "read_not_ok.csv",
-        ),
+    assert res.schema_ok == schema_ok, (
+        str(res.schema_errors["data"]) if res.schema_errors else None
     )
-
-    assert not res.read_ok
-    assert res.schema_ok, str(res.schema_errors["data"]) if res.schema_errors else None
