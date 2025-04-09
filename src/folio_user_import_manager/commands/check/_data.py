552e9f95e78a832bf7e5878bb22601cb731c2f17
Katherine Bargar <kbargar@fivecolleges.edu>
2025-04-09 15:42:23 -0400

    Add all validations for user import data (#5)
    I implemented all the fields in the mod-user-endpoint raml: https://github.com/folio-org/mod-user-import/blob/master/ramls/schemas/userdataimport.json. For the most part the Pandera checks were straightforward, though it gets weird when some fields are required in a not required parent object. Figuring out how Pandera worked was a whole other story but it is much better than validating by hand.
    
    There's a pile of test data and test cases. It's kind of magic how they get discovered but you can create a new test case by adding a properly named csv in the sample folder. The schema_ samples expect to have some sort of Pandera issues. There's a comment at the top of them for `column name|dataframe error|check name`. Usually you'll only need one of the dataframe or check.

--- src/folio_user_import_manager/commands/check/_data.py
+++ src/folio_user_import_manager/commands/check/_data.py
@@ -4,6 +4,7 @@ import pandera.polars as pla
 import polars as pl
 
 from ._models import CheckOptions
+from ._schema import UserImportSchema
 
 
 def run(
@@ -14,54 +15,42 @@ def run(
 ]:
     schema_errors: dict[str, pla.errors.SchemaErrors] = {}
     read_errors: dict[str, pl.exceptions.PolarsError] = {}
-    while True:
-        user_data_import_schema = pla.DataFrameSchema(
-            {
-                "username": pla.Column(
-                    str,
-                    description="A unique name belonging to a user. "
-                    "Typically used for login",
-                    unique=True,
-                ),
-                "externalSystemId": pla.Column(
-                    str,
-                    description="A unique ID that corresponds to an external authority",
-                    unique=True,
-                ),
-            },
-            strict=True,
-        )
 
-        for n, p in (
-            {"data": options.data_location}
-            if isinstance(options.data_location, Path)
-            else options.data_location
-        ).items():
-            try:
-                pl.read_csv(p)
-            except pl.exceptions.PolarsError as e:
+    for n, p in (
+        {"data": options.data_location}
+        if isinstance(options.data_location, Path)
+        else options.data_location
+    ).items():
+        try:
+            pl.read_csv(p, comment_prefix="#", try_parse_dates=True)
+        except pl.exceptions.PolarsError as e:
+            read_errors[n] = e
+
+        data: pl.DataFrame | None
+        try:
+            data = pl.read_csv(
+                p,
+                comment_prefix="#",
+                ignore_errors=True,
+                try_parse_dates=True,
+            )
+        except pl.exceptions.PolarsError as e:
+            if n not in read_errors:
                 read_errors[n] = e
+            continue
 
-            data: pl.DataFrame | None
-            try:
-                data = pl.read_csv(p, ignore_errors=True)
-            except pl.exceptions.PolarsError as e:
-                if n not in read_errors:
-                    read_errors[n] = e
-                continue
+        try:
+            UserImportSchema.validate(data, lazy=True)
+        except pla.errors.SchemaError as se:
+            schema_errors[n] = pla.errors.SchemaErrors(
+                UserImportSchema.to_schema(),
+                [se],
+                data,
+            )
+        except pla.errors.SchemaErrors as se:
+            schema_errors[n] = se
 
-            try:
-                user_data_import_schema.validate(data, lazy=True)
-            except pla.errors.SchemaError as se:
-                schema_errors[n] = pla.errors.SchemaErrors(
-                    user_data_import_schema,
-                    [se],
-                    data,
-                )
-            except pla.errors.SchemaErrors as se:
-                schema_errors[n] = se
-
-        return (
-            schema_errors if len(schema_errors) > 0 else None,
-            read_errors if len(read_errors) > 0 else None,
-        )
+    return (
+        schema_errors if len(schema_errors) > 0 else None,
+        read_errors if len(read_errors) > 0 else None,
+    )
--- tests/commands/check/test_data.py
+++ tests/commands/check/test_data.py
@@ -1,52 +1,71 @@
-import pytest
+from dataclasses import dataclass
+from pathlib import Path
+
+from pandera.polars import errors as ple
+from pytest_cases import parametrize, parametrize_with_cases
+
+_samples = list((Path() / "tests" / "commands" / "check" / "samples").glob("*.csv"))
 
 
-def test_check_data_ok(request: pytest.FixtureRequest) -> None:
+@dataclass
+class _SchemaErrors:
+    column: str | None = None
+    reason: str | ple.SchemaErrorReason | None = None
+    check_name: str | None = None
+
+
+class DataErrorCases:
+    @parametrize(csv=[s for s in _samples if "ok" in str(s)])
+    def case_ok(self, csv: Path) -> tuple[Path, bool, None]:
+        return (csv, True, None)
+
+    @parametrize(csv=[s for s in _samples if "read" in str(s)])
+    def case_bad_read(self, csv: Path) -> tuple[Path, bool, None]:
+        return (csv, False, None)
+
+    @parametrize(csv=[s for s in _samples if "schema" in str(s)])
+    def case_bad_schema(self, csv: Path) -> tuple[Path, bool, _SchemaErrors]:
+        with csv.open() as file:
+            params = [
+                p if len(p) > 0 else None
+                for p in file.readline().strip("#").strip("\n").split("|")
+            ]
+        return (csv, True, _SchemaErrors(*params))
+
+
+@parametrize_with_cases("path,read_expected,schema_expected", DataErrorCases)
+def test_check_data(
+    path: Path,
+    read_expected: bool,  # noqa: FBT001
+    schema_expected: _SchemaErrors | None,
+) -> None:
     import folio_user_import_manager.commands.check as uut
 
     res = uut.run(
-        uut.CheckOptions(
-            "",
-            "",
-            "",
-            "",
-            data_location=request.path.parent / "samples" / "ok.csv",
-        ),
+        uut.CheckOptions("", "", "", "", path),
     )
 
-    assert res.read_ok, str(res.read_errors["data"]) if res.read_errors else None
-    assert res.schema_ok, str(res.schema_errors["data"]) if res.schema_errors else None
-
-
-def test_check_schema_not_ok(request: pytest.FixtureRequest) -> None:
-    import folio_user_import_manager.commands.check as uut
-
-    res = uut.run(
-        uut.CheckOptions(
-            "",
-            "",
-            "",
-            "",
-            data_location=request.path.parent / "samples" / "schema_not_ok.csv",
-        ),
+    read_ok = res.read_ok
+    assert read_ok == read_expected, (
+        str(res.read_errors["data"]) if res.read_errors else None
     )
-
-    assert res.read_ok, str(res.read_errors["data"]) if res.read_errors else None
-    assert not res.schema_ok
-
-
-def test_check_read_not_ok(request: pytest.FixtureRequest) -> None:
-    import folio_user_import_manager.commands.check as uut
-
-    res = uut.run(
-        uut.CheckOptions(
-            "",
-            "",
-            "",
-            "",
-            data_location=request.path.parent / "samples" / "read_not_ok.csv",
-        ),
+    schema_ok = res.schema_ok
+    assert schema_ok == (schema_expected is None), (
+        str(res.schema_errors["data"]) if res.schema_errors else None
     )
+    if schema_expected and res.schema_errors:
+        assert len(res.schema_errors["data"].schema_errors) == 1
+        print(res.schema_errors["data"])  # noqa: T201
+        err = res.schema_errors["data"].schema_errors[0]
 
-    assert not res.read_ok
-    assert res.schema_ok, str(res.schema_errors["data"]) if res.schema_errors else None
+        if err.data is not None and schema_expected.column is not None:
+            assert err.data.column == schema_expected.column
+
+        if err.schema is not None and schema_expected.column is not None:
+            assert err.schema.name == schema_expected.column
+
+        if schema_expected.reason:
+            assert err.reason_code.name == schema_expected.reason
+
+        if schema_expected.check_name:
+            assert err.check.name == schema_expected.check_name
